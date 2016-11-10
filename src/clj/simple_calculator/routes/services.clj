7f658d8aa290cbd33bc342acaa4cbcd7a82e69e5
TashaGospel <nhanhnhanh17399@gmail.com>
2016-11-10 21:23:24 +1300

    Render result

--- src/clj/simple_calculator/routes/services.clj
+++ src/clj/simple_calculator/routes/services.clj
@@ -13,32 +13,8 @@
   (context "/api" []
     :tags ["thingie"]
 
-    (GET "/plus" []
-      :return       Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary      "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
-
-    (POST "/minus" []
-      :return      Long
-      :body-params [x :- Long, y :- Long]
-      :summary     "x-y with body-parameters."
-      (ok (- x y)))
-
-    (GET "/times/:x/:y" []
-      :return      Long
-      :path-params [x :- Long, y :- Long]
-      :summary     "x*y with path-parameters"
-      (ok (* x y)))
-
-    (POST "/divide" []
-      :return      Double
-      :form-params [x :- Long, y :- Long]
-      :summary     "x/y with form-parameters"
-      (ok (/ x y)))
-
-    (GET "/power" []
-      :return      Long
-      :header-params [x :- Long, y :- Long]
-      :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))))
+    (GET "/calculate" []
+      :return       s/Str
+      :query-params [string :- s/Str]
+      :summary "Calculate and simplify the expression"
+      (ok (str string "*1")))))
--- src/cljs/simple_calculator/main.cljs
+++ src/cljs/simple_calculator/main.cljs
@@ -1,49 +1,76 @@
 (ns simple-calculator.main
   (:require [reagent.core :as r :refer [atom]]
-            [simple-calculator.bootstrap-components :as c]))
+            [simple-calculator.bootstrap-components :as c]
+            [ajax.core :as ajax]))
 
 (def preview-id "preview")
+(def render-area-id "render-area")
 
-(defn- update-preview! [jax state]
-  (when (nil? @jax) (reset! jax (get (js/MathJax.Hub.getAllJax preview-id) 0)))
-  (js/MathJax.Hub.Queue (array "Text" @jax @state)))
+(defn- update-preview! [string]
+  (let [jax (get (js/MathJax.Hub.getAllJax preview-id) 0)]
+    (js/MathJax.Hub.Queue (array "Text" jax string))))
 
-(defn calculate [string]
-  string)
+(defn calculate [string callback]
+  (ajax/GET "/api/calculate"
+            {:params  {:string string}
+             :handler callback}))
+
+(defn render-math [string callback]
+  ; callback cannot be #(), must be (fn [])
+  (let [jax (get (js/MathJax.Hub.getAllJax render-area-id) 0)]
+    (js/console.log jax)
+    (js/MathJax.Hub.Queue (array "Text" jax string))
+    (js/MathJax.Hub.Queue (fn [] (callback
+                                   (.-innerHTML
+                                     (js/document.getElementById
+                                       render-area-id)))))))
+
+(defn add-input [inputs input input-rendered result result-rendered]
+  (swap! inputs conj {:input           input
+                      :input-rendered  input-rendered
+                      :result          result
+                      :result-rendered result-rendered}))
 
 (defn submit! [state inputs]
-  (let [html (.-innerHTML (js/document.getElementById preview-id))]
-    (swap! inputs conj {:input   @state
-                        :preview html
-                        :result  (calculate @state)})
+  (let [input-rendered (.-innerHTML (js/document.getElementById preview-id))
+        string @state]
+    (calculate string
+               (fn [result]
+                 (render-math result
+                              (fn [result-rendered]
+                                (add-input inputs
+                                           string input-rendered
+                                           result result-rendered)))))
     (reset! state "")))
 
 (defn input [state inputs]
-  (let [jax (atom nil)]
-    (fn []
-      [c/FormGroup
-       {:control-id "input"}
-       [(r/create-class
-          {:reagent-render
-           (fn []
-             [:input.form-control
-              {:type        :text
-               :value       @state
-               :on-change   #(do
-                              (reset! state (-> % .-target .-value)))
-               :on-key-down #(do
-                              (when (= 13 (.-keyCode %))
-                                (submit! state inputs)))}])
-           :component-did-update
-           #(update-preview! jax state)})]])))
+  (fn []
+    [c/FormGroup
+     {:control-id "input"}
+     [(r/create-class
+        {:reagent-render
+         (fn []
+           [:input.form-control
+            {:type        :text
+             :value       @state
+             :on-change   #(do
+                            (reset! state (-> % .-target .-value)))
+             :on-key-down #(do
+                            (when (= 13 (.-keyCode %))
+                              (submit! state inputs)))}])
+         :component-did-update
+         #(update-preview! @state)})]]))
 
-(defn preview []
+(defn render-area [id & {:keys [hidden?]}]
   [(r/create-class
      {:reagent-render
       (fn []
-        [:div {:id preview-id} "``"])
+        [:div
+         {:id    id
+          :class (when hidden? "hidden")}
+         "``"])
       :component-did-mount
-      #(js/MathJax.Hub.Queue (array "Typeset" js/MathJax.Hub preview-id))})])
+      #(js/MathJax.Hub.Queue (array "Typeset" js/MathJax.Hub id))})])
 
 (defn set-up-mathjax! []
   (js/MathJax.Hub.Config (js-obj "messageStyle" "none"))
@@ -55,35 +82,33 @@
     (fn []
       [c/Row
        [c/Col {:xs 6} [input state inputs]]
-       [c/Col {:xs 6} [preview]]])))
+       [c/Col {:xs 6} [render-area preview-id]]])))
 
 (defn past-inputs [inputs state]
   [:div
-   (for [{:keys [input preview result]} @inputs]
+   (for [{:keys [input input-rendered result result-rendered]} @inputs]
      ^{:key (rand-int js/Number.MAX_VALUE)}
      [c/Row
       {:class "text-center"}
       [:hr]
-      [c/Col
-       {:xs 6}
-       [:div
-        {:dangerouslySetInnerHTML {:__html preview}
-         :style                   {"overflow-x" "auto"
-                                   "overflow-y" "hidden"}}]
-       [:br]
-       [c/Button {:on-click #(reset! state input)} "Copy"]]
-      [c/Col
-       {:xs 6}
-       [:div
-        {:style {:overflow-x "auto"
-                 :overflow-y "hidden"}}
-        result]]])])
+      (for [[s s-rendered] [[input input-rendered] [result result-rendered]]]
+        (let [id (str (rand-int js/Number.MAX_VALUE))]
+          ^{:key id}
+          [c/Col
+           {:xs 6
+            :id id}
+           [:div
+            {:dangerouslySetInnerHTML {:__html s-rendered}
+             :style                   {:overflow-x "auto"
+                                       :overflow-y "hidden"}}]
+           [:br]
+           [c/Button {:on-click #(reset! state s)} "Copy"]]))])])
 
-; inputs: '({:input s/Str :preview s/Str :result s/Str})
 ; TODO: Add proper styling
 (defn main []
   (let [inputs (atom ())
         state (atom "")]
     [c/Grid
+     [render-area render-area-id :hidden? true]
      [input-and-preview inputs state]
      [past-inputs inputs state]]))
